77a2ad250d8aa7ee96a200a65875c0b785b9329c
Billy Buehl <billybuehl792@gmail.com>
2024-09-03 17:10:09 -0400

    setup

---
+++ app/(tabs)/profile.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const Profile = () => {
+  return (
+    <View className="flex-1 bg-red-800 mt-5">
+      <Text className="text-3xl mt-4">Profile</Text>
+    </View>
+  );
+};
+
+export default Profile;
+
+const styles = StyleSheet.create({});
